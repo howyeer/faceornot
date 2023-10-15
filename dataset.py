aa85b4267e1d7087d8a5aa249f3f2039cf886a11
howyeer <howyeer32@qq.com>
2023-10-15 18:59:52 +0800

    10.15

--- dataset.py
+++ dataset.py
@@ -15,20 +15,20 @@ else:
 
 train_data = []
 train_label = []
-with open("D:/aaacode/faceornot/train_data.csv", 'r', encoding='UTF-8', errors='ignore')as f, \
-     open("D:/aaacode/faceornot/train_label.csv", 'r', encoding='UTF-8', errors='ignore')as l:
+with open("D:/aaacode/faceornot/train_data.csv", 'r')as f, \
+     open("D:/aaacode/faceornot/train_label.csv", 'r')as l:
     reader_f = csv.reader(f)
     reader_l = csv.reader(l)
     for row in reader_f:
         train_data.append(row)
-        print(len(row))
     for label in reader_l:
         train_label.append(label)
        
 
-# for i, row in enumerate(train_data):
-#     array = np.array(row, dtype=np.float64)
-#     array = array*255
-#     img = array.reshape(16, 16)
-#     img_path = os.path.join("D:/aaacode/faceornot/img",  "{}.png".format(i))
-#     cv2.imwrite(img_path, img)
+for i, row in enumerate(train_data):
+    array = np.array(row, dtype=np.float64)
+    array = array*255
+    img = array.reshape(19, 19)
+    img = img.transpose(1,0)
+    img_path = os.path.join("D:/aaacode/faceornot/img",  "{}.png".format(i))
+    cv2.imwrite(img_path, img)
--- mlp.py
+++ mlp.py
@@ -1,68 +1,103 @@
 import numpy as np
+import torch
 import torch.nn as nn
+from torch.optim.lr_scheduler import StepLR
+from torch.optim.lr_scheduler import CosineAnnealingLR
 import csv
 import random
 import os 
 import cv2
 from PIL import Image
 
-train_data = []
-train_label = []
+epoch = 40
+hid1 = 100
+hid2 = 50
+group_num = 5
+dis_val = 0.6
+
+data = []
+label_list = []
 with open("D:/aaacode/faceornot/train_data.csv", 'r')as f, \
      open("D:/aaacode/faceornot/train_label.csv", 'r')as l:
     reader_f = csv.reader(f)
     reader_l = csv.reader(l)
     for row in reader_f:
-        train_data.append(row)
-        print(len(row))
+        data.append(np.array(row, dtype=np.float64))
     for label in reader_l:
-        train_label.append(label)
+        if int(label[0]) == 1:
+            label_list.append(1)
+        else:   
+            label_list.append(0)
 
+group_size = int(400/group_num)
+def random_group(arr, group_size):
+    random.shuffle(arr)
+    return [arr[i:i+group_size] for i in range(0, len(arr), group_size)]
 
-'''
-class Mlp(nn.Layer):
-    """ MLP module
-    Impl using nn.Linear and activation is GELU, dropout is applied.
-    Ops: fc -> act -> dropout -> fc -> dropout
+arr_1 = list(range(400))
+arr_0 = list(range(400,800))
+result_1 = random_group(arr_1, group_size)
+result_0 = random_group(arr_0, group_size)
+for j in range(group_num):
+    result_1[j].extend(result_0[j]) 
+result = result_1
 
-    Attributes:
-        fc1: nn.Linear
-        fc2: nn.Linear
-        act: GELU
-        dropout: dropout after fc
-    """
+class Mlp(nn.Module):
 
-    def __init__(self,
-                 embed_dim,
-                 mlp_ratio,
-                 dropout=0.):
+    def __init__(self, input, hidden1, hidden2, output, dropout=0.1):
         super().__init__()
-        w_attr_1, b_attr_1 = self._init_weights()
-        self.fc1 = nn.Linear(embed_dim,
-                             int(embed_dim * mlp_ratio),
-                             weight_attr=w_attr_1,
-                             bias_attr=b_attr_1)
-
-        w_attr_2, b_attr_2 = self._init_weights()
-        self.fc2 = nn.Linear(int(embed_dim * mlp_ratio),
-                             embed_dim,
-                             weight_attr=w_attr_2,
-                             bias_attr=b_attr_2)
+        self.hid1 = nn.Linear(input, hidden1, bias=True)
+        self.hid2 = nn.Linear(hidden1, hidden2, bias=True)
+        self.out = nn.Linear(hidden2, output, bias=True)
         self.act = nn.GELU()
         self.dropout = nn.Dropout(dropout)
 
-    def _init_weights(self):
-        weight_attr = paddle.ParamAttr(
-            initializer=paddle.nn.initializer.TruncatedNormal(std=0.2))
-        bias_attr = paddle.ParamAttr(
-            initializer=paddle.nn.initializer.Constant(0.0))
-        return weight_attr, bias_attr
-
     def forward(self, x):
-        x = self.fc1(x)
+        x = self.hid1(x)
         x = self.act(x)
         x = self.dropout(x)
-        x = self.fc2(x)
+        x = self.hid2(x)
+        x = self.act(x)
         x = self.dropout(x)
+        x = self.out(x)
         return x
-'''
+
+model = Mlp(361, hid1, hid2, 1)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+lr_rate = StepLR(optimizer, step_size=4, gamma=0.8, verbose=True)
+# cosine_lr = CosineAnnealingLR(optimizer, T_max=10, eta_min=5e-3, verbose=True) 
+loss_fn = torch.nn.BCELoss()
+fn = nn.Sigmoid()
+
+for n in range(group_num):
+    val_index = result.pop(n)
+    train_inex = np.array(result)
+    train_inex = train_inex.flatten()
+    print("第", n, "组")
+    acc_list = [[]]
+    for epoch_id in range(epoch):
+        model.train()
+        for index in list(train_inex):
+            predict = model(torch.Tensor(data[index]))
+            predict = fn(predict)
+            loss = loss_fn(predict, torch.Tensor(label_list[index]))
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        lr_rate.step()
+        if epoch_id%5 == 0:
+            print('epoch={}, loss={}'.format(epoch_id, loss.flatten().np()))
+        
+        model.eval()
+        print("模型评估")
+        i = 0
+        acc = 0
+        for index in val_index:
+            pre = model(data[index])
+            if pre >= dis_val: pre = 1
+            else: pre = 0
+            i += 1
+            if pre == label[index]:
+                acc +=1
+            print("acc={}".format(acc/i))
+            acc_list[n].append(acc/i)
